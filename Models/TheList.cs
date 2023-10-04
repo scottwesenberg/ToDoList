797dfdb2fef64484a8d99ff075a7a6fe5745ebd0
scottwesenberg <scowesen@gmail.com>
2023-10-04 15:36:27 -0400

    Add project files.

---
+++ Models/TheList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class TheList
+    {
+        [Required]
+        public int ID { get; set; }
+        [Required]
+
+        public string? Description { get; set; }
+
+        [Display(Name = "Created Date")]
+        [DataType(DataType.Date)]
+        public DateTime? CreatedDate { get; set; }
+    }
+}
